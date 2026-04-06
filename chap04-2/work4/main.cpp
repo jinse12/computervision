1df3a793a8b7710d0f396c0b09e8c8cad1e7e5bb
jinse12 <dtd1811@naver.com>
2026-04-06 12:22:02 +0900

    과제4

---
+++ chap04-2/work4/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(void)
+{
+	VideoCapture cap(0);
+	if (!cap.isOpened()) {
+		cerr << "Camera open failed!" << endl;
+		return -1;
+	}
+
+	int w = cvRound(cap.get(CAP_PROP_FRAME_WIDTH));
+	int h = cvRound(cap.get(CAP_PROP_FRAME_HEIGHT));
+
+	int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+	VideoWriter outputVideo("output.avi", fourcc, 30, Size(w, h));
+
+	if (!outputVideo.isOpened()) {
+		cout << "File open failed!" << endl;
+		return -1;
+	}
+
+	Mat frame;
+	while (true) {
+		cap >> frame;
+		if (frame.empty()) break;
+
+		outputVideo << frame;
+		imshow("Camera Recording", frame);
+
+		int key = waitKey(33);
+		if (key == 'q' || key == 'Q') {
+			cout << "녹화 종료." << endl;
+			break;
+		}
+	}
+}
