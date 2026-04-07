658abad8bf975de2b77bdc6592e1bd1e36da6fd2
jinse12 <dtd1811@naver.com>
2026-04-07 14:50:16 +0900

    과제4

--- chap04-3/work4/main.cpp
+++ chap04-3/work4/main.cpp
@@ -3,11 +3,9 @@ using namespace cv;
 
 int main()
 {
-	Mat src = imread("lenna.bmp", IMREAD_COLOR);    // 컬러 영상으로 읽기
+	Mat src = imread("lenna.bmp", IMREAD_COLOR);
 	if (src.empty()) return -1;
 
-	// 얼굴 주위에 파란색 원 그리기 (그림판으로 좌표 확인)
-	// 중심: (300, 275), 반지름: 119
 	circle(src, Point(300, 275), 119, Scalar(255, 0, 0), 2, LINE_AA);
 
 	imshow("image", src);
