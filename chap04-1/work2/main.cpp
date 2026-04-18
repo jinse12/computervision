36b1d950ad2e0d2e0c21b2ceb9313b5736c66dcb
jinse12 <dtd1811@naver.com>
2026-04-18 22:41:42 +0900

    과제2

--- chap04-1/work2/main.cpp
+++ chap04-1/work2/main.cpp
@@ -1,34 +1,18 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
-using namespace cv;
+#include <opencv2/opencv.hpp>
 using namespace std;
+using namespace cv;
 
-int main(void)
-{
+int main() {
 	VideoCapture cap(0);
-	if (!cap.isOpened()) {
-		cerr << "Camera open failed!" << endl;
-		return -1;
-	}
-	cout << "Frame width: " << cvRound(cap.get(CAP_PROP_FRAME_WIDTH)) << endl;
-	cout << "Frame height: " << cvRound(cap.get(CAP_PROP_FRAME_HEIGHT)) << endl;
-
-	Mat frame, bright;
-
 	while (true) {
+		Mat frame;
 		cap >> frame;
-		if (frame.empty()) {
-			cerr << "frame empty!" << endl;
-			break;
-		}
-		bright = frame + Scalar(100, 100, 100);
-
-		imshow("frame", frame);
+		Mat bright;
+		bright = frame+Scalar(100,100,100);
+		imshow("origin", frame);
 		imshow("bright", bright);
-
-		if (waitKey(10) == 27)
-			break;
+		if (waitKey(1) == 'q')break;
 	}
-
 	return 0;
 }
