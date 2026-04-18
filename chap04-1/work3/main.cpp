8584b60dee1db45dec2bb89bf8e242d1d84f2c14
jinse12 <dtd1811@naver.com>
2026-04-18 22:28:26 +0900

    과제3

--- chap04-1/work3/main.cpp
+++ chap04-1/work3/main.cpp
@@ -1,24 +1,18 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
-#include <iostream> 
 using namespace std;
 using namespace cv;
 
 int main() {
 	VideoCapture cap(0);
 	int count = 0;
-
 	while (true) {
 		Mat frame;
 		cap >> frame;
-
-		imshow("img", frame);
-
+		imshow("origin", frame);
 		int key = waitKey(1);
-		if (key == 'Q' || key == 'q') break;
-		if (key == 's') {
-			imwrite(format("frame%02d.jpg", count), frame);
-			count++;
-		}
+		if (key == 's') imwrite(format("frame%02d.jpg", count++), frame);
+		if (key == 'q')break;
 	}
 	return 0;
 }
