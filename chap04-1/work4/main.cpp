3a40697275f768846611fa3b2f10da8d6e3afae5
jinse12 <dtd1811@naver.com>
2026-04-18 22:17:33 +0900

    과제4

--- chap04-1/work4/main.cpp
+++ chap04-1/work4/main.cpp
@@ -1,25 +1,21 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
+using namespace std;
 using namespace cv;
 
 int main() {
-    VideoCapture cap(0);
-
-    while (true) {
-        Mat frame;
-        cap >> frame;
-
-        int h = frame.rows;
-        int w = frame.cols;
-
-        Mat cross = frame.clone();
-        line(cross, Point(w/2, 0), Point(w/2, h), Scalar(0, 0, 255), 2);
-        line(cross, Point(0, h/2), Point(w, h/2), Scalar(0, 0, 255), 2);
-
-        imshow("frame", frame);
-        imshow("cross", cross);
-
-        if (waitKey(1) == 'q') break;
-    }
-
-    return 0;
+	VideoCapture cap(0);
+	Mat frame;
+	while (true) {
+	cap >> frame;
+	Mat cross = frame.clone();
+	int h = cross.rows;
+	int w = cross.cols;
+		line(cross, Point(w / 2, 0), Point(w / 2, h), { 0,0,255 }, 1);
+		line(cross, Point(0, h / 2), Point(w, h / 2), { 0,0,255 }, 1);
+		imshow("origin", frame);
+		imshow("cross", cross);
+		if (waitKey(1) == 'q')break;
+	}
+	return 0;
 }
