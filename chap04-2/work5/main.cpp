c98996bdc6d84a045183a13efdbd5c70fb0d53d8
jinse12 <dtd1811@naver.com>
2026-04-18 23:18:32 +0900

    과제4

--- chap04-2/work5/main.cpp
+++ chap04-2/work5/main.cpp
@@ -33,3 +33,31 @@ int main(void)
 		imshow("Camera Recording", frame);
 	}
 	return 0;
+}
+
+/*
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main() {
+	VideoCapture cap("stopwatch.avi");
+	Mat frame;
+	cap >> frame;
+
+	int fourcc = VideoWriter::fourcc('m', 'p', '4', 'v');
+	VideoWriter writer;
+
+	while (true) {
+		cap >> frame;
+		if (frame.empty()) break;
+		imshow("origin", frame);
+		int key= waitKey(33);
+		if (key == 's') writer.open("gwage.mp4", fourcc, 30, Size(frame.cols, frame.rows));
+		if (writer.isOpened()) writer << frame;
+		if (key == 'e')break;
+	}
+	return 0;
+}
+*/
