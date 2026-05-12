a8b82449735badcbff6fc2c24a69515eec766e7e
jinse12 <dtd1811@naver.com>
2026-05-12 23:54:36 +0900

    실습과제4

---
+++ chap05-2/work4/main.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main(void)
+{
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	if (src.empty()) { cerr << "Image load failed!" << endl; return -1; }
+
+	float alpha = 1.f;
+	
+	Scalar avg = mean(src);
+	float mean_val = (float)avg[0];
+
+	Mat dst(src.rows, src.cols, src.type());
+
+	for (int j = 0; j < src.rows; j++) {
+		for (int i = 0; i < src.cols; i++) {
+			float val = src.at<uchar>(j, i) + (src.at<uchar>(j, i) - mean_val) * alpha;
+			dst.at<uchar>(j, i) = saturate_cast<uchar>(val);
+		}
+	}
+
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey();
+	return 0;
+}
