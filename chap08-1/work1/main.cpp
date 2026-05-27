d391f5a7ff388946469b96146e266f32cd8e137a
jinse12 <dtd1811@naver.com>
2026-05-27 15:42:31 +0900

    실습과제1

---
+++ chap08-1/work1/main.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main(void)
+{
+	Mat src = imread("tekapo.bmp");
+	if (src.empty()) { cerr << "Image load failed!" << endl; return -1; }
+
+	Point2f srcPts[3], dstPts[3];
+	srcPts[0] = Point2f(0, 0);
+	srcPts[1] = Point2f(src.cols - 1, 0);
+	srcPts[2] = Point2f(src.cols - 1, src.rows - 1);
+
+	dstPts[0] = Point2f(0, 0);
+	dstPts[1] = Point2f((src.cols - 1) / 2.f, 0);
+	dstPts[2] = Point2f((src.cols - 1) / 2.f, (src.rows - 1) / 2.f);
+
+	Mat M = getAffineTransform(srcPts, dstPts);
+	cout << M << endl;
+	Mat dst;
+	warpAffine(src, dst, M, Size());
+
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey();
+	return 0;
+}
