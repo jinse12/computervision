e16d66c28c99b122e0791eb5f8f6de3a7a386e84
jinse12 <dtd1811@naver.com>
2026-06-05 21:40:22 +0900

    실습과제2

---
+++ chap05-1/work2/main.cpp
@@ -0,0 +1,30 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+Mat src, dst;
+Point startPt;
+void onMouse(int event, int x, int y, int flags, void* username) {
+	if (event == EVENT_LBUTTONDOWN) {
+		startPt = Point(x, y);
+	}
+	else if (event == EVENT_LBUTTONUP) {
+		Point endPt(x, y);
+		int x1 = min(startPt.x, endPt.x);
+		int y1 = min(startPt.y, endPt.y);
+		int x2 = max(startPt.x, endPt.x);
+		int y2 = max(startPt.y, endPt.y);
+		Rect roi(x1, y1, x2 - x1, y2 - y1);
+		dst(roi) += 100;
+		imshow("dst", dst);
+	}
+}
+int main() {
+	src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	dst = src.clone();
+	namedWindow("dst");
+	setMouseCallback("dst", onMouse);
+	imshow("dst", dst);
+	waitKey();
+}
