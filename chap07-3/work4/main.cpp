e9f6db1e6e8c0e7e7c1bafc47614b2fc807d6d4b
jinse12 <dtd1811@naver.com>
2026-06-13 21:44:00 +0900

    실습과제4

--- chap07-3/work4/main.cpp
+++ chap07-3/work4/main.cpp
@@ -42,3 +42,39 @@ int main(void)
 	waitKey();
 	return 0;
 }
+
+//#include <opencv2/opencv.hpp>
+//#include <iostream>
+//using namespace cv;
+//using namespace std;
+//Mat src, dst;
+//Point startPt;
+//void onMouse(int event, int x, int y, int flags, void* userdata) {
+//	if (event == EVENT_LBUTTONDOWN) {
+//		startPt = Point(x, y);
+//	}
+//	else if (event == EVENT_LBUTTONUP) {
+//		Point endPt(x, y);
+//		int x1 = min(startPt.x, endPt.x);
+//		int y1 = min(startPt.y, endPt.y);
+//		int x2 = max(startPt.x, endPt.x);
+//		int y2 = max(startPt.y, endPt.y);
+//		Rect roi(x1, y1, x2 - x1, y2 - y1);
+//		Mat roiRegion = dst(roi);
+//		float data[] = { -1,-1,-1,-1,9,-1,-1,-1,-1 };
+//		Mat kernel(3, 3, CV_32FC1, data);
+//		filter2D(roiRegion, roiRegion, -1, kernel);
+//		imshow("dst", dst);
+//
+//	}
+//}
+//int main() {
+//	src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+//	dst = src.clone();
+//	namedWindow("dst");
+//	setMouseCallback("dst", onMouse);
+//	imshow("dst", dst);
+//	waitKey();
+//	return 0;
+//
+//}
