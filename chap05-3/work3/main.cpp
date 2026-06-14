93c4f70ac3bef476b50673f6366fe05a83a38116
jinse12 <dtd1811@naver.com>
2026-06-14 18:24:02 +0900

    실습과제3

--- chap05-3/work3/main.cpp
+++ chap05-3/work3/main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <opencv2/opencv.hpp>
-using namespace std;
+#include <iostream>
 using namespace cv;
+using namespace std;
+Mat src;
 Mat calcGrayHist(const Mat& img) {
 	Mat hist;
 	int channels[] = { 0 };
@@ -12,23 +13,24 @@ Mat calcGrayHist(const Mat& img) {
 	calcHist(&img, 1, channels, noArray(), hist, dims, histSize, ranges);
 	return hist;
 }
-Mat mygetGrayHistImage(const Mat& hist) {
-	double histMax;
-	minMaxLoc(hist, 0, &histMax);
+Mat getGrayHistImage(const Mat& hist) {
+	double histmax;
+	minMaxLoc(hist, 0, &histmax);
 	Mat imgHist(100, 256, CV_8UC1, Scalar(255));
 	for (int i = 0; i < 255; i++) {
-		int y1 = 100 - cvRound(hist.at<float>(i, 0) * 100 / histMax);
-		int y2 = 100 - cvRound(hist.at<float>(i + 1, 0) * 100 / histMax);
-		line(imgHist, Point(i, y1), Point(i + 1, y2), Scalar(0));
+		int y1 = 100 - cvRound(hist.at<float>(i, 0) * 100 / histmax);
+		int y2 = 100 - cvRound(hist.at<float>(i+1, 0) * 100 / histmax);
+		line(imgHist, Point(i, y1), Point(i+1, y2), Scalar(0));
+
 	}
 	return imgHist;
 }
 int main() {
-	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
-	imshow("입력영상", src);
+	src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	imshow("src", src);
 	Mat hist = calcGrayHist(src);
-	Mat histimg = mygetGrayHistImage(hist);
-	imshow("srchist", histimg);
+	Mat histImg = getGrayHistImage(hist);
+	imshow("histimg", histImg);
 	waitKey();
-	return;
+	return 0;
 }
